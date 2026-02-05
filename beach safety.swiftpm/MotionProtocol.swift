329ea35ff93de8d60cea9aa4c90b29cc5615a620
notsp1derman <yas.salamon@gmail.com>
2026-02-05 15:15:55 -0300

    arquivo inicial

---
+++ beach safety.swiftpm/MotionProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  MotionProtocol.swift
+//  beirutes_game
+//
+//  Created by Enzo Enrico on 10/11/25.
+//
+
+import Foundation
+
+protocol MotionProtocol {
+	func startListening()
+	func stopListening()
+}
